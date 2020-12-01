55cbd25697cccd78dd52faed9d045f180c474689
ArnavNawal <snawal34@gmail.com>
2020-12-01 20:54:27 +0530

    car racing stage 2

--- js/Game.js
+++ js/Game.js
@@ -15,12 +15,41 @@ class Game {
     });
   }
 
-  start(){
+  async start(){
     if(gameState === 0){
       player = new Player();
-      player.getCount();
+      var playerCountRef = await database.ref('playerCount').once("value")
+      if(playerCountRef.exists()){
+        playerCount = playerCountRef.val()
+        player.getCount();
+      }
+      
       form = new Form()
       form.display();
+    }
+  }
+  play(){
+    form.hide();
+    text("game start",120,100)
+    Player.getPlayerInfo();
+    if(allPlayers !== undefined){
+      var displayPos = 130;
+      for(var plr in allPlayers){
+        if(plr === "player"+player.index){
+          fill("red");
+        }
+        else{
+          fill("black")
+        }
+      displayPos = displayPos+20;
+      text(allPlayers[plr].name+":"+allPlayers[plr].distance,120,displayPos);
+      
+      }
+      
+    }
+    if(keyIsDown(UP_ARROW) && player.index !== null){
+      player.distance = player.distance+50
+      player.update();
     }
   }
 }
